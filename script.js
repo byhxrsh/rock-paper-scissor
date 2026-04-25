22191e14b8016681b765ee31c6078b23ad94ba2b
harsh89-ai <220668533+harsh89-ai@users.noreply.github.com>
2026-04-25 23:17:45 +0530

    remove un-useful comments.

--- script.js
+++ script.js
@@ -12,9 +12,8 @@
 //   ~ if random string is `paper` and user's choice is `scissor` return `woo-hoo, you won!`   
 //   ~ if random string is `scissor` and user's choice is `paper` return `you loose! try again?`   
 
-// create a function that will randomly return any of the three string values- `rock`, `paper`, or `scissor`.
 let getComputerChoice = function() {
-  if (Math.random() < 0.32) {
+  if (Math.random() < 1/3) {
     return `paper`
   } else if (Math.random() > 0.32 && Math.random() < 0.64) {
     return `rock`
@@ -23,7 +22,6 @@ let getComputerChoice = function() {
   }
 };
 
-// create a function to get the user's input/choice. 
 let getUserInput = function() {
   let userChoice = prompt(`what's your choice?`, ``)
   return userChoice
@@ -33,28 +31,25 @@ let getUserInput = function() {
 let humanScore = 0;
 let computerScore = 0;
 
-
 function playGame() {
-  
-  // function with logic of first round
   function playRound(humanChoice, computerChoice) {
-    const normalizedHumanChoice = humanChoice.toLowerCase(); // normalizes uppercase/mixed input to lowercase 
+    const normalizedHumanChoice = humanChoice().toLowerCase();
     
-    if (normalizedHumanChoice === computerChoice) {
+    if (normalizedHumanChoice === computerChoice()) {
       console.log(`Draw! let's play again.`);
-    } else if (normalizedHumanChoice === `rock` && computerChoice === `scissor`) {
+    } else if (normalizedHumanChoice === `rock` && computerChoice() === `scissor`) {
       ++humanScore;
       console.log(`you won! rock beats scissor. you won ${humanScore}/5 rounds.`);
-    } else if (normalizedHumanChoice === `rock` && computerChoice === `paper`) {
+    } else if (normalizedHumanChoice === `rock` && computerChoice() === `paper`) {
       ++computerScore;
       console.log(`you lose! paper beats rock. you won ${humanScore}/5 rounds.`);
-    } else if (normalizedHumanChoice === `paper` && computerChoice === `rock`) {
+    } else if (normalizedHumanChoice === `paper` && computerChoice() === `rock`) {
       ++humanScore;
       console.log(`you won! paper beats rock. you won ${humanScore}/5 rounds.`);
-    } else if (normalizedHumanChoice === `paper` && computerChoice === `scissor`) {
+    } else if (normalizedHumanChoice === `paper` && computerChoice() === `scissor`) {
       ++computerScore;
       console.log(`you lose! scissor beats paper. you won ${humanScore}/5 rounds.`);
-    } else if (normalizedHumanChoice === `scissor` && computerChoice === `rock`) {
+    } else if (normalizedHumanChoice === `scissor` && computerChoice() === `rock`) {
       ++computerScore
       console.log(`you lose! rock beats scissor. you won ${humanScore}/5 rounds.`);
     } else {
@@ -63,11 +58,11 @@ function playGame() {
     }
   }
 
-  playRound(getUserInput(), getComputerChoice());
-  playRound(getUserInput(), getComputerChoice());
-  playRound(getUserInput(), getComputerChoice());
-  playRound(getUserInput(), getComputerChoice());
-  playRound(getUserInput(), getComputerChoice());
+  playRound(getUserInput, getComputerChoice);
+  playRound(getUserInput, getComputerChoice);
+  playRound(getUserInput, getComputerChoice);
+  playRound(getUserInput, getComputerChoice);
+  playRound(getUserInput, getComputerChoice);
   
   if (humanScore > computerScore) {
     console.log(`yup! FINAL SCORE ~ you won ${humanScore} out of 5 rounds.`)
